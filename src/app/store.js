6aa2c04ef410c4023a10413f817bff1fd6b4c4dc
phantomcorn <jj.phantakorn@outlook.com>
2025-02-20 21:37:47 +0700

    enable cartReducer method

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import authReducer from "../features/auth/authSlice"
+import cartReducer from "../features/cart/cartSlice"
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        cart: cartReducer,
         [apiSlice.reducerPath] : apiSlice.reducer
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
