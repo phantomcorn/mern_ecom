ce9ece01d6183dfcadae695b02c8484dbc15faa0
phantomcorn <jj.phantakorn@outlook.com>
2025-02-20 21:38:10 +0700

    update Main and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/Main';
-import VerifyEmail from './pages/VerifyEmail';
 import NoPage from './pages/NoPage';
-import Dashboard from './pages/Dashboard';
+import Products from './pages/Products.jsx';
+import Product from './pages/Product.jsx';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import Prefetch from './features/auth/Prefetch.jsx';
-import PersistLogin from './features/auth/PersistLogin.jsx';
+import Prefetch from './features/cart/Prefetch.jsx';
+import PersistCart from './features/cart/PersistCart.jsx';
 
 
 function App() {
@@ -15,17 +15,17 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
           <Routes>
-            {/* Authenticated path */}
-            <Route element={<PersistLogin/>}>
+            <Route element={<PersistCart/>}>
               {/* Persists data for every child page under Prefetch*/}
               <Route element={<Prefetch/>}> 
-                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="/product" element={<Products />} />
+                <Route path="/product/:id" element={<Product />} />
+                <Route path="/" element={<Main />} />
+                <Route index element={<Main />} />
               </Route>
             </Route>
         
-            <Route path="/" element={<Main />} />
-            <Route index element={<Main />} />
-            <Route path="verify-email" element={<VerifyEmail />} />
+            
             {/* Fallback route for unmatched paths */}
             <Route path="*" element={<NoPage />} />
    
