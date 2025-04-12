5967f2c077f6c6ee0be9072f8abfec5636400288
phantomcorn <jj.phantakorn@outlook.com>
2025-04-12 18:25:07 +0700

    use cart to checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,15 +5,18 @@ import {
 } from '@stripe/react-stripe-js';
 import { useStartCheckoutMutation } from '../features/checkout/checkoutApiSlice';
 import { useCallback } from 'react';
+import { useSelector } from "react-redux";
+import { selectCurrCart } from "../features/cart/cartSlice";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_FRONTEND_KEY)
 
 export default function Checkout() {
-
+    
+    const cart = useSelector(selectCurrCart)
     const [startCheckout, {props}] = useStartCheckoutMutation()
     const fetchClientSecret = useCallback(() => {
         try {
-            const resp = startCheckout().unwrap().then((data) => data.clientSecret)
+            const resp = startCheckout({cart}).unwrap().then((data) => data.clientSecret)
             return resp
         } catch (err) {
             console.log(err)
