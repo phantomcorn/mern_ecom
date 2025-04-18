92678b29be99812276f78bae08dfed00a0ec1202
phantomcorn <jj.phantakorn@outlook.com>
2025-04-18 23:39:04 +0700

    thai baht conversion

--- src/features/product/priceConversion.js
+++ src/features/product/priceConversion.js
@@ -1,10 +1,11 @@
 const POUND_TO_PENNY = 100
-const unitToPound = (unitAmt) => {
-    return unitAmt / POUND_TO_PENNY
-}
+const STANG_TO_BAHT = 100
+const unitToPound = (unitAmt) => unitAmt / POUND_TO_PENNY
+const unitToBaht = (unitAmt) => unitAmt / STANG_TO_BAHT
 
 export default function getPrice(currency, unitAmt) {
     switch (currency) {
         case "gbp": return "£" + unitToPound(unitAmt)
+        case "thb": return "฿" + unitToBaht(unitAmt)
     }
 }
