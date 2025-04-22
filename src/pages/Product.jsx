c4a4f9fa745e4aa52eb8ef352554e3eb0392db87
phantomcorn <jj.phantakorn@outlook.com>
2025-04-22 16:37:41 +0700

    display loading not error

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -11,50 +11,49 @@ export default function Product() {
   const dispatch = useDispatch()
   const {id} = useParams(); // Extracts "67af5949ffde5f37c751f47d"
   const [quantity, setQuantity] = useState(1)
-  const [addToCart, {isUninitialized, isLoading, isError, isSuccess}] = useAddToCartMutation()
-  const productResp = useGetProductQuery({id}) /* Retrieves product information by Id*/
+  const [addToCart, cartMutation] = useAddToCartMutation()
+  const {data, isLoading, isError, isSuccess} = useGetProductQuery({id}) /* Retrieves product information by Id*/
 
-
-
-  if (productResp.isSuccess) {
-
-    const product = productResp.data
-    const priceObj = product.prices[0]
-    const priceId = priceObj.id
-    const price = getPrice(priceObj.currency,priceObj.unit_amount)
-
-    const handleClick = (e) => {
-      e.preventDefault()
-      try {
-        const resp = addToCart({id, priceId, quantity}).unwrap()
-        const products = resp.products
-        dispatch(setCart({products}))
-      } catch (err) {
-        console.log(err)
-      } 
-    }
-
-    return (
-      <div>
-        <h1>Product Page</h1>
-        <p>Product ID:{product.id} </p>
-        <p>Price: {price}</p>
-        <button onClick={handleClick}>
-          {isUninitialized && "Add to cart"}
-          {isLoading && "Adding..."}
-          {isSuccess && "Added! Add more?"}
-        </button>
-        <div>
-          <button onClick={() => setQuantity((prev) => Math.max(prev - 1, 1))}>-</button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => Math.min(prev + 1, 10))}>+</button>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div> Product not found </div>
-    )
+  if (isLoading) {
+    return (<div> Loading...</div>)
   }
-  
+
+  if (isError) {
+    return (<div> Product not found </div>)
+  }
+
+  const product = data
+  const priceObj = product.prices[0]
+  const priceId = priceObj.id
+  const price = getPrice(priceObj.currency,priceObj.unit_amount)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    try {
+      const resp = addToCart({id, priceId, quantity}).unwrap()
+      const products = resp.products
+      dispatch(setCart({products}))
+    } catch (err) {
+      console.log(err)
+    } 
+  }
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <p>Product ID:{product.id} </p>
+      <p>Price: {price}</p>
+      <button onClick={handleClick}>
+        {cartMutation.isUninitialized && "Add to cart"}
+        {cartMutation.isLoading && "Adding..."}
+        {cartMutation.isSuccess && "Added! Add more?"}
+      </button>
+      <div>
+        <button onClick={() => setQuantity((prev) => Math.max(prev - 1, 1))}>-</button>
+        {quantity}
+        <button onClick={() => setQuantity((prev) => Math.min(prev + 1, 10))}>+</button>
+      </div>
+    </div>
+  );
+
 };
