4d8d855a68d0b9d0414c3495398cf26fdcd0c5bb
phantomcorn <jj.phantakorn@outlook.com>
2025-04-25 17:43:44 +0700

    admin: create product

--- backend/controllers/admin/productController.js
+++ backend/controllers/admin/productController.js
@@ -9,10 +9,35 @@ import asyncHandler from 'express-async-handler'
 import jwt from "jsonwebtoken"
 import Product from '../../models/productModel.js'
 import bcrypt from "bcrypt"
+import stripe from '../../db/stripe.js'
 // @route POST /api/admin/product/add
 const add = asyncHandler(async (req, res) => {
     console.log("adding product")
-    return res.status(200).json({})
+
+    const {name, quantity, price, description, tax_code} = req.body
+    console.log(req.body)
+    const productObj = await stripe.products.create({
+        name,
+        description,
+        tax_code
+    })
+    if (!productObj) return res.status(401).json({message: "Couldn't create product (Stripe)"})
+    const priceObj = await stripe.prices.create({
+        currency: "gbp",
+        unit_amount: price * 100,
+        product: productObj.id
+    })
+    if (!priceObj) return res.status(401).json({message: "Couldn't create pricing (Stripe)"})
+
+    // For inventory
+    await Product.create({
+        productId: productObj.id,
+        priceId: priceObj.id,
+        name,
+        quantity
+    })
+
+    return res.status(200).json({message: "Product created!"})
 })
 
 // @route POST /api/admin/product/delete
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -6,12 +6,13 @@ import mongoose from "mongoose"
 
 //model for database
 const productSchema = new mongoose.Schema({
-    name : {type: String , required: true},
-    description : {type: String, required: true},
+    productId : {type: String , required: true},
+    priceId: {type: String, required: true},
+    name : {type: String, required: true},
     quantity: {type: Number, required: true},
 })
 
-//REST API on ThreadsDB.products
+//REST API on ThreadsDB.products (Inventory keepsake)
 const Product = mongoose.model("product", productSchema)
 
 export default Product;
