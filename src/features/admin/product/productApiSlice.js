a0db9e251c757ae88b103d972f989f275f56c81a
phantomcorn <jj.phantakorn@outlook.com>
2025-04-26 14:18:51 +0700

    frontend apislice

--- src/features/admin/product/productApiSlice.js
+++ src/features/admin/product/productApiSlice.js
@@ -5,9 +5,32 @@ const productApiSlice = apiSlice.injectEndpoints({
         adminGetProducts : builder.query({
             query: () => "/api/admin/product"
         }),
+        adminAddProduct : builder.mutation({
+            query: payload => ({
+                url: "/api/admin/product/add",
+                method: "POST",
+                body: payload
+            })
+        }),
+        adminDeleteProduct : builder.mutation({
+            query: payload => ({
+                url: "/api/admin/product/delete",
+                method: "POST",
+                body: {id : payload.id}
+            })
+        }),
+        adminUpdateQuantity : builder.mutation({
+            query: payload => ({
+                url: "/api/admin/product/update",
+                method: "POST",
+                body: {id : payload.id, quantity : payload.quantity}
+            })
+        }),
     })
 })
 
 export const {
-    useAdminGetProductsQuery
-} = productApiSlice
+    useAdminGetProductsQuery,
+    useAdminDeleteProductMutation, 
+    useAdminAddProductMutation, 
+    useAdminUpdateQuantityMutation} = productApiSlice
